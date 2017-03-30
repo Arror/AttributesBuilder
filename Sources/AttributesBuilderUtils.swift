638fa792104b39793c4ad3c7c8d84c2929443fd9
Maqiang <maqiang@ezbuy.com>
2017-03-30 18:22:31 +0800

    Support NSRegularExpression

--- Sources/AttributesBuilderUtils.swift
+++ Sources/AttributesBuilderUtils.swift
@@ -1,8 +1,24 @@
 import Foundation
 
 extension NSRange {
+    
+    func toCountableRange() -> CountableRange<Int> {
+        return self.location..<(self.location + self.length)
+    }
+    
     static func from(countableRange range: CountableRange<Int>) -> NSRange {
         return NSMakeRange(range.lowerBound, range.upperBound - range.lowerBound)
+    }
+}
+
+extension String {
+    
+    var fullRange: CountableRange<Int> {
+        return 0..<self.characters.count
+    }
+    
+    var fullNSRange: NSRange {
+        return NSMakeRange(0, self.characters.count)
     }
 }
 
