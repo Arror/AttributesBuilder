2ad0acf5b0ab5bf6144488d7063fe13e3a81f2a4
Arror <hallo.maqiang@gmail.com>
2017-06-14 19:42:15 +0800

    Add String and AttributeString combine method

--- Sources/AttributesBuilderUtils.swift
+++ Sources/AttributesBuilderUtils.swift
@@ -31,3 +31,17 @@ public func +(lhs: NSAttributedString, rhs: NSAttributedString) -> NSAttributedS
     s.append(rhs)
     return s
 }
+
+public func +(lhs: String, rhs: NSAttributedString) -> NSAttributedString {
+    
+    let s = NSMutableAttributedString(string: lhs)
+    s.append(rhs)
+    return s
+}
+
+public func +(lhs: NSAttributedString, rhs: String) -> NSAttributedString {
+    
+    let s = NSMutableAttributedString(attributedString: lhs)
+    s.append(NSAttributedString(string: rhs))
+    return s
+}
