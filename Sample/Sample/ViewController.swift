2bc87559e2b6d417cd0979c1528e296549ed1047
Arror <hallo.maqiang@gmail.com>
2017-06-19 14:59:57 +0800

    Update sample

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -8,26 +8,38 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let content = "CocoaPods is a dependency manager for Swift and Objective-C Cocoa projects. It has over 30 thousand libraries and is used in over 1.9 million apps. CocoaPods can help you scale your projects elegantly."
+        let linkStr = "https://github.com/Arror"
         
-        let builder1 = AttributesBuilder {
-            $0.color(content.contains("CocoaPods") ? .blue : .yellow)
+        let content = """
+        CocoaPods is a dependency manager for Swift and Objective-C Cocoa projects.
+        
+        Github: \(linkStr)
+        """
+        
+        let whole = AttributesBuilder {
+            $0.color(.gray)
             $0.font(.systemFont(ofSize: 14.0))
         }
         
-        let builder2 = builder1.copy {
+        let link = AttributesBuilder {
+            $0.color(.blue)
+            $0.link(URL(string: linkStr)!)
+        }
+        
+        let first = whole.copy {
             $0.font(.systemFont(ofSize: 24.0))
         }
         
-        let builder3 = AttributesBuilder {
+        let mark = AttributesBuilder {
             $0.color(.red)
             $0.font(.boldSystemFont(ofSize: 18.0))
         }
         
         self.contentLabel.attributedText = content
-            .rs.rendered(by: builder1)
-            .rs.rendered(by: builder2, range: 0..<1)
-            .rs.rendered(by: builder3, regexPattern: "Swift and Objective-C")
+            .rs.rendered(by: whole)
+            .rs.rendered(by: first, range: 0..<1)
+            .rs.rendered(by: link, regexPattern: linkStr)
+            .rs.rendered(by: mark, regexPattern: "Swift and Objective-C Cocoa", options: [.caseInsensitive])
     }
 }
 
