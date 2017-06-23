24327188a74940febc6fed65ddec3f8c8ddea074
Arror <hallo.maqiang@gmail.com>
2017-06-23 10:49:58 +0800

    Update sample

--- Sample/Sample/ViewController.swift
+++ Sample/Sample/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
             $0.link(URL(string: linkStr)!)
         }
         
-        let first = whole.copy {
+        let special = whole.copy {
             $0.color(.red)
             $0.font(.systemFont(ofSize: 24.0))
         }
@@ -40,14 +40,14 @@ class ViewController: UIViewController {
             $0.font(.boldSystemFont(ofSize: 30.0))
         }
         
-        let r1 = content.startIndex..<(content.index(after: content.startIndex))
+        let specialRange1 = content.startIndex..<(content.index(after: content.startIndex))
         
-        let r2 = content.index(content.startIndex, offsetBy: 10)..<content.index(content.startIndex, offsetBy: 12)
+        let specialRange2 = content.index(content.startIndex, offsetBy: 10)..<content.index(content.startIndex, offsetBy: 12)
         
         self.contentLabel.attributedText = content
             .rs.rendered(by: whole)
-            .rs.rendered(by: first, ranges: r1, r2)
-            .rs.rendered(by: link, regexPattern: linkStr)
+            .rs.rendered(by: special, ranges: specialRange1, specialRange2)
+            .rs.rendered(by: link, ranges: content.range(of: linkStr)!)
             .rs.rendered(by: mark, regexPattern: "✨")
     }
 }
